1efb7c50f1dd96eea8c185c413bd5d08bffc1457
puerarongo <akhardin96@gmail.com>
2022-04-12 21:19:09 +0300

    portal

--- src/components/modal/Modal.jsx
+++ src/components/modal/Modal.jsx
@@ -1,14 +1,36 @@
-import React from "react";
+import React, { Component } from "react";
+// ? import PORTAL
+import {createPortal} from 'react-dom';
 import styles from "./Modal.module.css";
 
-const Modal = ({ value }) => {
-    return (
-        <div className={styles.overlay}>
-            <div className={styles.modal}>
-                <img className={styles.img} src={value.big} alt={value.id} />
-            </div>
-        </div>
-    )
-}
+
+const modalRoot = document.querySelector("#modal__root")
+
+class Modal extends Component {
+    componentDidUpdate(prevProps, prevState) {
+        console.log("its update")
+
+        window.addEventListener("keydown", e => {
+            if (e.code === "Escape") {}
+         });
+    }
+
+    componentWillUnmount() {
+        console.log("its unmount")
+    }
+
+    render() {
+        const { id, big } = this.props.value;
+
+        return createPortal(
+            <div className={styles.overlay}>
+                <div className={styles.modal}>
+                    <img className={styles.img} src={big} alt={id} />
+                </div>
+            </div>,
+            modalRoot
+        );
+    };
+};
 
 export default Modal;
