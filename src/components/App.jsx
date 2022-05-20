7757d92668d9a4e542abc073ec40c4ed2dae5429
puerarongo <akhardin96@gmail.com>
2022-05-20 17:46:46 +0300

    add husky

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,89 +1,91 @@
-import React, { Component } from "react";
-import Searchbar from "./searchbar/Searchbar";
-import ImageGallery from "./imageGallery/ImageGallery";
-import ImageGalleryItem from "./imageGalleryItem/ImageGalleryItem";
-import Loader from "./loader/Loader";
-import Button from "./button/Button";
-import Modal from "./modal/Modal";
+import React, { Component } from 'react';
+import Searchbar from './searchbar/Searchbar';
+import ImageGallery from './imageGallery/ImageGallery';
+import ImageGalleryItem from './imageGalleryItem/ImageGalleryItem';
+import Loader from './loader/Loader';
+import Button from './button/Button';
+import Modal from './modal/Modal';
 
-import fetchFunc from "funcFiles/fetchFunc";
+import fetchFunc from 'funcFiles/fetchFunc';
 
-class App extends Component { 
+class App extends Component {
   state = {
     response: [],
     value: null,
     page: 0,
     error: null,
-    status: "idle",
+    status: 'idle',
     showModal: false,
-    modalValue: []
+    modalValue: [],
   };
 
   // ? func
   componentDidUpdate(prevProps, prevState) {
     if (prevState.value !== this.state.value) {
-      this.setState({response: []});
+      this.setState({ response: [] });
       this.requestHandler();
+    } else {
+      return;
     }
-    else {
-      return
-    }
-  };
+  }
 
-  createArr = (value) => {
+  createArr = value => {
     const newArr = value.hits.map(elem => {
-      return { id: elem.id, small: elem.webformatURL, big: elem.largeImageURL }
-    })
+      return { id: elem.id, small: elem.webformatURL, big: elem.largeImageURL };
+    });
 
     this.setState(prevState => {
       return {
         response: [...prevState.response, ...newArr],
-        status: "resolved"
-      }
-    })
+        status: 'resolved',
+      };
+    });
   };
 
-
-  requestHandler =  () => {
+  requestHandler = () => {
     const { value, page } = this.state;
 
-    this.setState({ status: "pending" });
+    this.setState({ status: 'pending' });
 
-    fetchFunc(value, page).then(pictures => this.createArr(pictures)).catch(error => {
-      console.log(error)
-      this.setState({status: "rejected"});
-    })
-      .finally(() => this.setState(prevS => { return { page: prevS.page + 1 } }))
-  }  
-  
+    fetchFunc(value, page)
+      .then(pictures => this.createArr(pictures))
+      .catch(error => {
+        console.log(error);
+        this.setState({ status: 'rejected' });
+      })
+      .finally(() =>
+        this.setState(prevS => {
+          return { page: prevS.page + 1 };
+        })
+      );
+  };
 
-  searchImg = (value) => {
+  searchImg = value => {
     this.setState(prevState => {
       if (prevState.value !== value) {
         return {
           page: 1,
-          value: value
-        }
+          value: value,
+        };
       }
 
-      return { value: value }
+      return { value: value };
     });
   };
 
-  modalHandler = (id) => {
-    const imgId = this.state.response.find(elem => elem.id === id)
-    this.setState({ modalValue: imgId })
-    this.modalOpen()
-  }
+  modalHandler = id => {
+    const imgId = this.state.response.find(elem => elem.id === id);
+    this.setState({ modalValue: imgId });
+    this.modalOpen();
+  };
 
   modalOpen = () => {
-    this.setState({ showModal: true })
+    this.setState({ showModal: true });
   };
 
   modalClose = () => {
-    this.setState({ showModal: false })
+    this.setState({ showModal: false });
   };
-
 
   // ? func
   render() {
@@ -91,25 +93,19 @@ class App extends Component {
 
     return (
       <div>
-        {showModal && (
-          <Modal value={modalValue} funcClose={this.modalClose}/>
-        )}
+        {showModal && <Modal value={modalValue} funcClose={this.modalClose} />}
         <Searchbar submit={this.searchImg} />
-        
+
         <ImageGallery>
           <ImageGalleryItem response={response} modal={this.modalHandler} />
         </ImageGallery>
 
-        {status === "pending" && (
-          <Loader />
-        )}
+        {status === 'pending' && <Loader />}
 
-        {response.length > 0 && (
-          <Button pressMore={this.requestHandler}/>
-        )}
+        {response.length > 0 && <Button pressMore={this.requestHandler} />}
       </div>
-    )
-  };
-};
+    );
+  }
+}
 
 export default App;
--- src/components/searchbar/Searchbar.jsx
+++ src/components/searchbar/Searchbar.jsx
@@ -1,54 +1,52 @@
-import React, { Component } from "react";
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import styles from "./Searchbar.module.css";
+import styles from './Searchbar.module.css';
 
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
 
 class Searchbar extends Component {
-    state = { text: "" };
+  state = { text: '' };
 
-    inputHandler = (e) => {
-        const { value } = e.currentTarget
-        this.setState({ text: value.toLowerCase() });
-    };
+  inputHandler = e => {
+    const { value } = e.currentTarget;
+    this.setState({ text: value.toLowerCase() });
+  };
 
+  submitHandler = e => {
+    e.preventDefault();
+    if (this.state.text.trim() === '') {
+      return Notify.warning('Please, enter the word into the search engine');
+    }
+    this.props.submit(this.state.text);
+    this.setState({ text: '' });
+  };
 
-    submitHandler = (e) => {
-        e.preventDefault();
-        if (this.state.text.trim() === "") {
-            return Notify.warning("Please, enter the word into the search engine");
-        }
-        this.props.submit(this.state.text);
-        this.setState({ text: "" });
-    };
+  render() {
+    const { text } = this.state;
 
-
-    render() {
-        const { text } = this.state;
-
-        return (
-        <header className={styles.header}>
-            <form className={styles.form} onSubmit={this.submitHandler}>
-                <button type="submit" className={styles.button}>
-                    <span className="button-label">Search</span>
-                </button>
-                <input
-                    className={styles.form__input}
-                    type="text"
-                    autoComplete="off"
-                    autoFocus
-                    placeholder="Search images and photos"
-                    value={text}
-                    onChange={this.inputHandler}
-                />
-            </form>
-        </header>
-        );
-    };
-};
+    return (
+      <header className={styles.header}>
+        <form className={styles.form} onSubmit={this.submitHandler}>
+          <button type="submit" className={styles.button}>
+            <span className="button-label">Search</span>
+          </button>
+          <input
+            className={styles.form__input}
+            type="text"
+            autoComplete="off"
+            autoFocus
+            placeholder="Search images and photos"
+            value={text}
+            onChange={this.inputHandler}
+          />
+        </form>
+      </header>
+    );
+  }
+}
 
 Searchbar.propTypes = {
-    submit: PropTypes.func.isRequired
+  submit: PropTypes.func.isRequired,
 };
 
 export default Searchbar;
